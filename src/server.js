3506f95d52e2defb445f82e0177d39768fd650cb
tetiana_hushnovska <thushnyovska@gmail.com>
2025-07-05 23:40:05 +0100

    add get routs

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import pino from 'pino';
 import pinoHttp from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './service/contacts.js';
 
 const PORT = getEnvVar('PORT') || 5543;
 
@@ -22,9 +23,33 @@ export const setupServer = () => {
 
     app.use(pinoHttp({ logger }));
 
-    app.get('/', (req, res) => {
+    app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
+
         res.json({
-            message: "Hello, world!"
+            status: 200,
+            message: "Successfully found contacts!",
+            data: contacts,
+        });
+    });
+
+    app.get('/contacts/:contactId', async (req, res, next) => {
+        const {contactId} = req.params;
+        const contact = await getContactById(contactId);
+
+        if (!contact) {
+            res
+                .status(404)
+                .json({
+                    message: 'Contact not found'
+                });
+            return;
+        }
+
+        res.json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}!`,
+            data: contact,
         });
     });
 
