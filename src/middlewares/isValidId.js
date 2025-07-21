5698b8f7562999f426c927b07babb474c0f15fc5
tetiana_hushnovska <thushnyovska@gmail.com>
2025-07-21 20:48:25 +0100

    fix issue with isValidId

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,8 +2,8 @@ import createHttpError from "http-errors";
 import { isValidObjectId } from "mongoose";
 
 export const isValidId = (req, res, next) => {
-    const { contactId } = req.parems;
-    if (!isValidObjectId(contactId)) {
+    const { id } = req.params;
+    if (!isValidObjectId(id)) {
         throw createHttpError(404, 'Bad request');
     }
 
